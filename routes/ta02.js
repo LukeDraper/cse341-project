574b3766691c9b85a838790933f7b0e1966bd058
LukeDraper <lukewdraper@gmail.com>
2021-09-21 17:01:51 -0600

    Team Activity 2

--- routes/ta02.js
+++ routes/ta02.js
@@ -3,13 +3,40 @@
 const express = require('express');
 const router = express.Router();
 
+const nameList = ['Peter', 'James', 'John'];
+let message = "";
+
 router.get('/', (req, res, next) => {
-  res.render('pages', {
+  res.render('pages/ta02', {
     title: 'Team Activity 02',
     path: '/ta02', // For pug, EJS
     activeTA03: true, // For HBS
     contentCSS: true, // For HBS
+    nameArray: nameList,
+    message: message
   });
+});
+
+router.post('/addUser', (req, res, next) => {
+  message = "";
+  const index = nameList.indexOf(req.body.nameInput);
+  if(index > -1) { 
+    message = "No clones allowed."
+  } else {
+    nameList.push(req.body.nameInput)
+  }
+  res.redirect('/ta02');
+});
+
+router.post('/removeUser', (req, res, next) => {
+  message = "";
+  const index = nameList.indexOf(req.body.nameRemove);
+  if(index > -1) { 
+    nameList.splice(index, 1);
+  } else {
+    message = "No such person."
+  }
+  res.redirect('/ta02');
 });
 
 module.exports = router;
